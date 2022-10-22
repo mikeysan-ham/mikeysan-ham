f6a6d8204b64ab6a0e5d96b8e02be0ef7bb1ba35
mikeysan-ham <104935198+mikeysan-ham@users.noreply.github.com>
2022-10-22 19:36:54 +0530

    Add files via upload
    a recursion program in c implementing the "Fibonacci" series

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fibo(int n )
+    {
+      if(n==0)
+      {
+      return 0; 
+      }
+      else if(n==1)
+      {
+      return 1;
+      }
+      else{
+        return (fibo(n-1)+fibo(n-2));
+      }
+    }
+int main(){
+    printf("Fibonacchi Series in 8th position  : ");
+    int z=fibo(8);
+    printf("%d",z);
+    return 0;
+}
